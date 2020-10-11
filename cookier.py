df5e33d2f400fe5e0c6232f5479540f423f41634
g3ntl3m3n <somethingsmeels@gmail.com>
2020-10-11 23:20:10 +0300

    ll

--- cookier.py
+++ cookier.py
@@ -10,17 +10,17 @@ class Cookier:
 
         req = requests.get(target)
 
-        print req.cookies
+        print(req.cookies)
         cookies = dict(admin=True)
         cookie_req = requests.get(target, cookies=cookies)
 
         for cookie in req.cookies:
-                print "Name:", cookie.name
-                print "Value:", cookie.value
-                print "Domain:", cookie.domain
-                print "Path:", cookie.path
-                print "Secure", cookie.secure
-                print "HTTP", self.check_http_only(cookie)
+                print("Name:", cookie.name)
+                print("Value:", cookie.value)
+                print("Domain:", cookie.domain)
+                print("Path:", cookie.path)
+                print("Secure", cookie.secure)
+                print("HTTP", self.check_http_only(cookie))
 
     def check_http_only(self, xd):
         if 'http_only' in xd.__rest.keys():
